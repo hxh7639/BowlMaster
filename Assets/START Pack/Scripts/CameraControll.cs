e42b66fe529149b541d982ec74d1a3cf1b229800
AcerTouch\Andy <AcerLaptop>
2017-02-09 14:12:36 -0600

    BM191 End

--- Assets/START Pack/Scripts/CameraControll.cs
+++ Assets/START Pack/Scripts/CameraControll.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class CameraControll : MonoBehaviour {
+
+	public Ball ball;
+
+	private Vector3 offset;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		offset = ball.rigidbody  // offset is ball position - 20 in Y direction
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate
 	}
+
+
 }
