fabde4a7447fd8a4ceac6a5623e70589072e6a1e
AcerTouch\Andy <AcerLaptop>
2017-02-15 14:27:59 -0600

    BM 199-1

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public float standingThreshold;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public bool isStanding(){
+		Vector3 rotationInEuler = transform.rotation.eulerAngles;
+		float tiltInX = Mathf.Abs(rotationInEuler.x);
+		float tiltInY = Mathf.Abs(rotationInEuler.y);
+
+		if (tiltInX <= standingThreshold) {
+			return true;
+		} else if (tiltInX <= standingThreshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
