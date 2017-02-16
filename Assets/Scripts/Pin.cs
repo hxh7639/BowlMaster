1f7dc0b693271b6e8f8d7c65638285e42777d665
Andy <hxh7639@yahoo.com.tw>
2017-02-15 22:40:27 -0600

    BM 200

--- Assets/Scripts/Pin.cs
+++ Assets/Scripts/Pin.cs
@@ -11,22 +11,21 @@ public class Pin : MonoBehaviour {
 		
 	}
 
-	public bool isStanding(){
-		Vector3 rotationInEuler = transform.rotation.eulerAngles;
-		float tiltInX = Mathf.Abs(rotationInEuler.x);
-		float tiltInY = Mathf.Abs(rotationInEuler.y);
-
-		if (tiltInX <= standingThreshold) {
-			return true;
-		} else if (tiltInX <= standingThreshold) {
+	public bool IsStanding(){
+		Vector3 greenVector = transform.up;
+		float standThreshold = standingThreshold / 10;
+		float x = Mathf.Abs (greenVector.x);
+		float z = Mathf.Abs (greenVector.z);
+		if (x < standThreshold && z < standThreshold) {
 			return true;
 		} else {
 			return false;
 		}
-	}
+
+}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		print (name + IsStanding());
 	}
 }
