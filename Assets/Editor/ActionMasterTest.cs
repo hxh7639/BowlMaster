df0f4ebc9e380d0d0fae35fce678d6b86f3b3adc
AcerTouch\Andy <AcerLaptop>
2017-02-24 13:49:08 -0600

    BM 213 - 1

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 	[TestFixture]
 	public class ActionMasterTest {
 		private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
-	
+		private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
 		[Test]
 		public void PassingTest() {
 			Assert.AreEqual (1,1);
@@ -17,6 +17,11 @@ using System.Collections.Generic;
 		public void T01OneStrikeReturnsEndTurn(){
 			ActionMaster actionMaster = new ActionMaster();
 			Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+		}
 
+		[Test]
+		public void T02Bowl8ReturnsTidy(){
+			ActionMaster actionMaster = new ActionMaster ();
+		Assert.AreEqual (tidy, actionMaster.Bowl (8));
 		}
 	}
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,10 +6,28 @@ public class ActionMaster{
 
 	public enum Action {Tidy, Rest, EndTurn, EndGame};
 
+	// private int[] bowls = new int[21];
+	private int bowl = 1;
 
 	public Action Bowl (int pins){
-		if(pins == 10){
+
+		if (pins < 0 || pins > 10) {
+			throw new UnityException ("Not sure what to return!");
+		}
+
+		// Other behaviour
+
+		if (pins == 10) {
 			return Action.EndTurn;
-		} else return Action.Tidy;
+		}
+
+		// If its the first bowl of a frame
+		// return Action.Tidy
+		if(bowl % 2 == 0){   // End of frame
+			
+		}
+
+		throw new UnityException ("Not sure what to return!");
 	}
+
 }
