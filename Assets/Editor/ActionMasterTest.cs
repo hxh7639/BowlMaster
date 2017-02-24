68880ae609abd3d10c2974f27817d9c4088fb638
Andy <hxh7639@yahoo.com.tw>
2017-02-23 21:55:35 -0600

    BM 212

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+	[TestFixture]
+	public class ActionMasterTest {
+		private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	
+		[Test]
+		public void PassingTest() {
+			Assert.AreEqual (1,1);
+		}
+
+		[Test]
+		public void T01OneStrikeReturnsEndTurn(){
+			ActionMaster actionMaster = new ActionMaster();
+			Assert.AreEqual (endTurn, actionMaster.Bowl(10));
+
+		}
+	}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster{
+
+	public enum Action {Tidy, Rest, EndTurn, EndGame};
+
+
+	public Action Bowl (int pins){
+		if(pins == 10){
+			return Action.EndTurn;
+		} else return Action.Tidy;
+	}
+}
