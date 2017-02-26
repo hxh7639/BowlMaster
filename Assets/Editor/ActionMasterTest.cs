bdd39ff206553636e0508e184e42a46ff1e5e1bf
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-02-26 16:39:01 -0600

    BM 215 end

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -8,6 +8,8 @@ using System.Collections.Generic;
 public class ActionMasterTest {
 	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
 	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Rest;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
     private ActionMaster actionMaster;
 
     [SetUp]
@@ -35,4 +37,61 @@ public class ActionMasterTest {
         actionMaster.Bowl(8);
         Assert.AreEqual(endTurn, actionMaster.Bowl(2));
     }
+
+    [Test]
+    public void T04CheckResetAtStrikeInLastFrame(){
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T05CheckResetAtStrikeInLastFrame(){
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        actionMaster.Bowl(1);
+        Assert.AreEqual(reset, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T06YouTubeRollsEndInEndGame(){
+        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        Assert.AreEqual(endGame, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T07GameEndAtBowl20(){
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        Assert.AreEqual(endGame, actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void T08Strikeon19TidyOn20(){
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        Assert.AreEqual(tidy, actionMaster.Bowl(5));
+    }
+
+    [Test]
+    public void T09BensBowl20Test(){
+        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10};
+        foreach (int roll in rolls){
+            actionMaster.Bowl(roll);
+        }     
+        Assert.AreEqual(tidy, actionMaster.Bowl(0));
+    }
+
+
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,16 +6,44 @@ public class ActionMaster{
 
 	public enum Action {Tidy, Rest, EndTurn, EndGame};
 
-	// private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins){
 
-		if (pins < 0 || pins > 10) {
-			throw new UnityException ("Not sure what to return!");
-		}
+		if (pins < 0 || pins > 10) {throw new UnityException ("Not sure what to return!1");}
 
-		// Other behaviour, e.g. last frame
+        bowls[bowl - 1] = pins;
+
+        if (bowl == 21){
+            return Action.EndGame;
+        }
+
+        //19
+        if (bowl == 19 && Bowl21Awarded())
+        {
+            bowl += 1;
+            return Action.Rest;
+        }else if (bowl == 19 && !Bowl21Awarded())
+        {
+            bowl += 1;
+            return Action.Tidy;
+        }
+
+        //20
+        if (bowl == 20 && !Bowl21Awarded())
+        {
+            return Action.EndGame;
+        }
+        else if (bowl == 20 && Bowl21AwardedOn19())
+        {
+            bowl += 1;
+            return Action.Tidy;
+        }
+        else if (bowl == 20){
+            bowl += 1;
+            return Action.Rest;
+        }
 
 		if (pins == 10) {
             bowl += 2;
@@ -36,7 +64,17 @@ public class ActionMaster{
 
 
 
+
 		throw new UnityException ("Not sure what to return!");
 	}
+
+    private bool Bowl21AwardedOn19(){
+        // Remember that arrays start counting at 0
+        return (bowls [19-1] >=10);
+    }
+
+    private bool Bowl21Awarded(){
+        return (bowls [19-1] + bowls [20-1] >=10);
+    }
 
 }
