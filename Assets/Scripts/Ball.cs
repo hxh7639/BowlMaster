0f7c3d2ae223eb956ca9c98badc2375c1d8872bb
AcerTouch\Andy <AcerLaptop>
2017-03-01 13:51:41 -0600

    BM 220-1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -33,6 +33,7 @@ public class Ball : MonoBehaviour {
         rigidBody.useGravity = false;
         rigidBody.velocity = Vector3.zero; // I used "new Vector3 (0,0,0);" instead
         rigidBody.angularVelocity = Vector3.zero;
+		gameObject.transform.rotation = Quaternion.identity;
 }
 
 
---
+++ Assets/Scripts/ScoreMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster {
+
+	public static List<int> ScoreFrames (List<int> rolls){
+		List<int> frameList = new List<int> ();
+
+		// Your code here
+
+		return frameList;
+	
+	}
+
+}
