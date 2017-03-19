372ca053e12414aa1d6af7504d6988ea74dfef5c
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-03-19 15:45:36 -0500

    BM 223 challenge done.

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -5,91 +5,91 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-[TestFixture]
-public class ActionMasterTest {
-
-	private List<int> pinFalls;
-
-	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
-	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
-    private ActionMaster.Action reset = ActionMaster.Action.Reset;
-    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
-
-    [SetUp]
-    public void Setup(){
-		pinFalls = new List<int> ();
-    }
-
-    [Test]
-	public void PassingTest() {
-	    Assert.AreEqual (1,1);
-	}
-
-	[Test]
-	public void T01OneStrikeReturnsEndTurn(){
-		pinFalls.Add (10);
-		Assert.AreEqual (endTurn, ActionMaster.NextAction(pinFalls));
-	}
-
-	[Test]
-	public void T02Bowl8ReturnsTidy(){
-		pinFalls.Add (8);
-		Assert.AreEqual (tidy, ActionMaster.NextAction(pinFalls));
-	}
-
-    [Test]
-    public void T03Bowl28ReturnsEndTurn(){
-		int[] rolls = { 2, 8 };
-		Assert.AreEqual(endTurn, ActionMaster.NextAction(rolls.ToList())); // turn array into list
-    }
-
-    [Test]
-    public void T04CheckResetAtStrikeInLastFrame(){
-        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
-		Assert.AreEqual(reset, ActionMaster.NextAction(rolls.ToList()));
-
-    }
-
-    [Test]
-    public void T05CheckResetAtStrikeInLastFrame(){
-        int[] rolls = { 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,9};
-		Assert.AreEqual(reset, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-    [Test]
-    public void T06YouTubeRollsEndInEndGame(){
-        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2, 9};
-		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-    [Test]
-    public void T07GameEndAtBowl20(){
-        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1};
-		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-    [Test]
-    public void T08Strikeon19TidyOn20(){
-        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,5};
-		Assert.AreEqual(tidy, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-    [Test]
-    public void T09BensBowl20Test(){
-        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,0};
-		Assert.AreEqual(tidy, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-	[Test]
-    public void T10BowlSpare(){
-		int[] rolls = { 0, 10, 5, 1};
-		Assert.AreEqual(endTurn, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-	[Test]
-    public void T11ThreeStrikesAtTheEnd(){
-        int[] rolls = {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1,10,10,10};
-		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
-    }
-
-}
+//[TestFixture]
+//public class ActionMasterTest {
+//
+//	private List<int> pinFalls;
+//
+//	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+//	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+//    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+//    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+//
+//    [SetUp]
+//    public void Setup(){
+//		pinFalls = new List<int> ();
+//    }
+//
+//    [Test]
+//	public void PassingTest() {
+//	    Assert.AreEqual (1,1);
+//	}
+//
+//	[Test]
+//	public void T01OneStrikeReturnsEndTurn(){
+//		pinFalls.Add (10);
+//		Assert.AreEqual (endTurn, ActionMaster.NextAction(pinFalls));
+//	}
+//
+//	[Test]
+//	public void T02Bowl8ReturnsTidy(){
+//		pinFalls.Add (8);
+//		Assert.AreEqual (tidy, ActionMaster.NextAction(pinFalls));
+//	}
+//
+//    [Test]
+//    public void T03Bowl28ReturnsEndTurn(){
+//		int[] rolls = { 2, 8 };
+//		Assert.AreEqual(endTurn, ActionMaster.NextAction(rolls.ToList())); // turn array into list
+//    }
+//
+//    [Test]
+//    public void T04CheckResetAtStrikeInLastFrame(){
+//        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10};
+//		Assert.AreEqual(reset, ActionMaster.NextAction(rolls.ToList()));
+//
+//    }
+//
+//    [Test]
+//    public void T05CheckResetAtStrikeInLastFrame(){
+//        int[] rolls = { 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,9};
+//		Assert.AreEqual(reset, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//    [Test]
+//    public void T06YouTubeRollsEndInEndGame(){
+//        int[] rolls = { 8, 2, 7, 3, 3, 4, 10, 2, 8, 10, 10, 8, 0, 10, 8, 2, 9};
+//		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//    [Test]
+//    public void T07GameEndAtBowl20(){
+//        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1};
+//		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//    [Test]
+//    public void T08Strikeon19TidyOn20(){
+//        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,5};
+//		Assert.AreEqual(tidy, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//    [Test]
+//    public void T09BensBowl20Test(){
+//        int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,0};
+//		Assert.AreEqual(tidy, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//	[Test]
+//    public void T10BowlSpare(){
+//		int[] rolls = { 0, 10, 5, 1};
+//		Assert.AreEqual(endTurn, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//	[Test]
+//    public void T11ThreeStrikesAtTheEnd(){
+//        int[] rolls = {1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1,10,10,10};
+//		Assert.AreEqual(endGame, ActionMaster.NextAction(rolls.ToList()));
+//    }
+//
+//}
--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,13 +4,97 @@ using UnityEngine;
 
 public class ScoreMaster {
 
+    // Returns a list of cumulative scores, like a normal score card.
+    public static List<int> ScoreCumulative (List<int> rolls){
+        List<int> cumulativeScores = new List<int>();
+        int runningTotal = 0;
+
+        foreach (int frameScore in ScoreFrames(rolls)){
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+    }
+
+    // Return a list of individual frame scores, NOT cumulative.
 	public static List<int> ScoreFrames (List<int> rolls){
-		List<int> frameList = new List<int> ();
+        List<int> frame = new List<int>();
 
-		// Your code here
+//        for (int i = 1; i < rolls.Count; i += 2)  // Qui#2 working
+//        {
+//            if (frame.Count < 10) // test if last frame
+//            {
+//                if ((rolls[i - 1] + rolls[i]) < 10) //test if open frame 
+//                { 
+//                    frame.Add(rolls[i - 1] + rolls[i]);
+//                }
+//                else // strike or spare
+//                {
+//                    if (i + 1 < rolls.Count)  //tests if next roll exist 
+//                    {
+//                        frame.Add(rolls[i - 1] + rolls[i] + rolls[i + 1]);
+//                        if (rolls[i - 1] == 10) // test if strike
+//                        {
+//                            i--; // strike should only advance i by 1 roll
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return frame;
+//    }
 
-		return frameList;
-	
-	}
+        for (int i = 1; i < (rolls.Count+1); i++) // andy
+        {
+            if (i % 2 == 0) // scoring every 2 rolls, if not 2 rolls yet just return
+            frame.Add (rolls[i-1] + rolls [i]);
+        }
+        return frame;
+
+    }
+
+//        for (int i = 0; i < rolls.Count; i += 2)  // Class
+//        {
+//            if (rolls[i - 1] == 10 )
+//            {
+//                break;
+//            }else if ((rolls[i-1] + rolls [i]) == 10){
+//                break;
+//            }else if ((rolls[i-1] + rolls [i]) == 10){
+//                frame.Add (rolls[i-1] + rolls [i] + rolls [i+1]);
+//            }else
+//
+//                frame.Add (rolls[i-1] + rolls [i]);
+//        }
+//        return frame;
+//
+//    }
+
+
+
+
+  /*      for(int i = 0; i < rolls.Count; i++) { // Qui's for loop, currently at test 21
+            if(i+1<rolls.Count) { //tests if the next roll exists in order to score the frame
+                if(rolls[i]!=10&&(rolls[i]+rolls[i+1])!=10){ //tests if a frame is not a strike or a spare
+                    frame.Add(rolls[i]+rolls[i+1]);
+                    i++;
+                } 
+                else {
+                    if(i+2<rolls.Count) { //tests if the first roll of the next frame exists in order to score the strike or spare
+                        frame.Add(rolls[i]+rolls[i+1]+rolls[i+2]);
+                        if ((rolls[i]+rolls[i+1])==10){
+                            i++;
+                        }
+
+                    }
+                }
+             }
+        }
+        return frame;
+    }
+    */
+
+
 
 }
