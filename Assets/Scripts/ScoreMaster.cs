92e1b2ee014e326997f2b288bd9c4e9cafb83c9a
Andy <hxh7639@yahoo.com.tw>
2017-03-20 22:06:43 -0500

    BM 223 Andy T13

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -47,16 +47,17 @@ public class ScoreMaster {
 
         for (int i = 0; i < (rolls.Count); i++) // andy
         {
-			if(rolls [i] >=10) //strike at current frame
-			{i++;
-			return frame;}
-			if(i>0 && rolls[i-1] + rolls [i] >=10) //spare at current frame (this would add the wrong frames together atm.)
-			{return frame;}
 
-			if (i > 1 && rolls [i - 2] == 10 || i > 1 && rolls [i - 1] + rolls [i] >= 10) {// handle pervious strike/spare
-				frame.Add (rolls [i - 2] + rolls [i - 1] + rolls [i]);
-			} else if (i > 0 && (i-1) % 2 ==0) {
+			if(i > 0 && (i-1) % 2 ==0){
+				if(i>0 && (rolls[i-1] + rolls [i]) <10) //if no spare
+				{
 				frame.Add (rolls [i - 1] + rolls [i]);
+				}
+			}else if (i > 1 && rolls [i - 2] == 10 ) {// handle pervious strike
+				frame.Add (rolls [i - 2] + rolls [i - 1] + rolls [i]);
+				frame.Add (rolls [i - 1] + rolls [i]);
+			}else if(i > 1 && (rolls [i - 2] + rolls [i-1]) >= 10){ //handle pervious spare
+				frame.Add (rolls [i - 2] + rolls [i - 1]+ rolls [i]);
 			}
         }
         return frame;
